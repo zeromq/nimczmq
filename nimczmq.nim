607578d6032820b2bd9ca186f5193696d9eb1a1a
Brian Knox <taotetek@gmail.com>
2016-12-18 21:13:47 -0500

    Problem: function arg names did not match C code

--- nimczmq.nim
+++ nimczmq.nim
@@ -35,9 +35,9 @@ type
     PSock* = ptr TSock
 
 proc zsock_new*(sockType: cint): PSock {.importc: "zsock_new".}
-proc zsock_connect*(sock: PSock, endpoint: cstring): cint {.importc: "zsock_connect".}
-proc zsock_bind*(sock: PSock, endpoint: cstring): cint {.importc: "zsock_bind".}
-proc zsock_destroy*(sock_p: pointer) {.importc: "zsock_destroy".}
+proc zsock_connect*(self: PSock, endpoint: cstring): cint {.importc: "zsock_connect".}
+proc zsock_bind*(self: PSock, endpoint: cstring): cint {.importc: "zsock_bind".}
+proc zsock_destroy*(self_p: pointer) {.importc: "zsock_destroy".}
 
 type
     TFrame {.final, pure.} = object
@@ -46,15 +46,15 @@ type
 proc zframe_new*(data: cstring, size: int): PFrame {.importc: "zframe_new".}
 proc zframe_new_empty*(): PFrame {.importc: "zframe_new_empty".}
 proc zframe_from*(str: cstring): PFrame {.importc: "zframe_from".}
-proc zframe_size*(frame: PFrame): cint {.importc: "zframe_size".}
-proc zframe_dup*(frame: PFrame): PFrame {.importc: "zframe_dup".}
-proc zframe_strdup*(frame: PFrame): cstring {.importc: "zframe_strdup".}
-proc zframe_send*(frame_p: pointer, sock: PSock, flags: int): cint {.importc: "zframe_send".}
-proc zframe_recv*(sock: PSock): PFrame {.importc: "zframe_recv".}
-proc zframe_destroy*(frame_p: pointer) {.importc: "zframe_destroy".}
+proc zframe_size*(self: PFrame): cint {.importc: "zframe_size".}
+proc zframe_dup*(self: PFrame): PFrame {.importc: "zframe_dup".}
+proc zframe_strdup*(self: PFrame): cstring {.importc: "zframe_strdup".}
+proc zframe_send*(self_p: pointer, sock: PSock, flags: int): cint {.importc: "zframe_send".}
+proc zframe_recv*(self: PSock): PFrame {.importc: "zframe_recv".}
+proc zframe_destroy*(self_p: pointer) {.importc: "zframe_destroy".}
 
-proc zstr_send*(sock: PSock, str: cstring): cint {.importc: "zstr_send".}
-proc zstr_recv*(sock: PSock): cstring {.importc: "zstr_recv".}
-proc zstr_free*(zstr_p: pointer) {.importc: "zstr_free".}
+proc zstr_send*(self: PSock, str: cstring): cint {.importc: "zstr_send".}
+proc zstr_recv*(self: PSock): cstring {.importc: "zstr_recv".}
+proc zstr_free*(self_p: pointer) {.importc: "zstr_free".}
 
 {.pop.}
